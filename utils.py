7b151929f55b3331514bc3bba5286ef94fb2faf8
RishabhKatiyar <rishabh.katiyar@outlook.com>
2021-04-19 23:04:26 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -102,3 +102,81 @@ def create_folders_and_copy_files(folder_tree, source_path, destination_path):
                 import shutil
                 print(f"Copying file .. {os.path.join(source_path, day_file_pair.filename)} to {file_path}")
                 shutil.copy(os.path.join(source_path, day_file_pair.filename), file_path)
+
+
+def get_folder_tree_for_videos(path, list_of_files):
+    """populating the data structure which holds folder structure metadata"""
+    folder_tree = {}
+    DayFilePair = namedtuple('DayFilePair', ['day', 'filename'])
+
+    for file_name in list_of_files:
+        file_name_date = file_name.split(sep="_")[1]
+        _year = int(file_name_date[0:4])
+        _month = int(file_name_date[4:6])
+        _day = int(file_name_date[6:8])
+
+        day_file_pair = DayFilePair(_day, file_name)
+
+        if _year in folder_tree:
+            if _month in folder_tree[_year]:
+                folder_tree[_year][_month].add(day_file_pair)
+            else:
+                folder_tree[_year][_month] = set([day_file_pair])
+        else:
+            folder_tree[_year] = {}
+            folder_tree[_year][_month] = set([day_file_pair])
+
+    return folder_tree
+
+
+def create_folders_and_copy_files_for_videos(folder_tree, source_path, destination_path):
+    """create year, month and dates folders as necessary
+        then paste the files in respective folders"""
+    for year_key in folder_tree:
+        file_path = os.path.join(destination_path, str(year_key))
+        if os.path.exists(file_path):
+            pass
+        else:
+            print(f"Creating path {file_path}")
+            os.mkdir(file_path)
+
+        for month_key in folder_tree[year_key]:
+            for month_enum_val in Months:
+                if month_enum_val == month_key:
+                    month_str = str(month_enum_val).split(sep=".")[1]
+
+            if month_key >= 10:
+                month_str_key = str(month_key) + " (" + month_str + ")"
+            else:
+                month_str_key = "0" + str(month_key) + " (" + month_str + ")"
+
+            file_path = os.path.join(destination_path, str(year_key), str(month_str_key))
+            if os.path.exists(file_path):
+                pass
+            else:
+                print(f"Creating path {file_path}")
+                os.mkdir(file_path)
+
+            for day_file_pair in folder_tree[year_key][month_key]:
+                if day_file_pair.day >= 10:
+                    day_str_key = str(day_file_pair.day) + " " + month_str
+                else:
+                    day_str_key = "0" + str(day_file_pair.day) + " " + month_str
+                file_path = os.path.join(destination_path, str(year_key), str(month_str_key), str(day_str_key))
+                if os.path.exists(file_path):
+                    pass
+                else:
+                    print(f"Creating path {file_path}")
+                    os.mkdir(file_path)
+
+                file_path = os.path.join(destination_path, str(year_key), str(month_str_key), str(day_str_key), "Videos")
+                if os.path.exists(file_path):
+                    pass
+                else:
+                    print(f"Creating path {file_path}")
+                    os.mkdir(file_path)
+
+                # copy the file to created or existing folder
+                import shutil
+                print(f"Copying file .. {os.path.join(source_path, day_file_pair.filename)} to {file_path}")
+                shutil.copy(os.path.join(source_path, day_file_pair.filename), file_path)
