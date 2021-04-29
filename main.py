c0e1c64679dcf7ee493fc5309cde9ef02cb72d39
RishabhKatiyar <rishabh.katiyar@outlook.com>
2021-04-30 00:19:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@
 
 import glob
 import os
-from utils import get_folder_tree, create_folders_and_copy_files, get_folder_tree_for_videos, create_folders_and_copy_files_for_videos
+from utils import get_folder_tree, create_folders_and_copy_files, get_folder_tree_with_name, create_folders_and_copy_files_for_videos
 
 
 # Press the green button in the gutter to run the script.
@@ -15,19 +15,22 @@ if __name__ == '__main__':
     source_path = input()
     '''
     # to be set by user
-    source_path = "D:\\One plus data\\Camera"
+    source_path = "D:\\dump"
     destination_path = "D:\\Pictures\\Pictures\\1. Home"
 
-    # getting list of files non recursive in given source_path
     os.chdir(source_path)
-    list_of_files = glob.glob("*.jpg")
-    # done
 
-    folder_tree = get_folder_tree(source_path, list_of_files)
+    # Photos ...
+    list_of_files = glob.glob("*.jpg")
+
+    # folder_tree = get_folder_tree(source_path, list_of_files)
+    # create_folders_and_copy_files(folder_tree, source_path, destination_path)
+
+    folder_tree = get_folder_tree_with_name(source_path, list_of_files)
     create_folders_and_copy_files(folder_tree, source_path, destination_path)
 
-    print("Processing Videos ...")
-    list_of_files = glob.glob("*.mp4")
+    # Videos ...
+    # list_of_files = glob.glob("*.mp4")
 
-    folder_tree = get_folder_tree_for_videos(source_path, list_of_files)
-    create_folders_and_copy_files_for_videos(folder_tree, source_path, destination_path)
+    # folder_tree = get_folder_tree_with_name(source_path, list_of_files)
+    # create_folders_and_copy_files_for_videos(folder_tree, source_path, destination_path)
