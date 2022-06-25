c6aee8ff1044ddbe7e8736662ddff5cd9cf9cd67
RishabhKatiyar <rishabh.katiyar@outlook.com>
2022-06-25 18:35:39 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
-	"fmt"
 	"os"
 	"path/filepath"
+	"time"
+	"errors"
 
 	"github.com/RishabhKatiyar/PhotoManager/utils"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"github.com/rs/zerolog/pkgerrors"
 )
 
 var (
@@ -17,12 +21,47 @@ var (
 )
 
 func main() {
+	start := time.Now()
+	// UNIX Time is faster and smaller than most timestamps
+	//zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	zerolog.ErrorStackMarshaler = pkgerrors.MarshalStack
+	//Set log level
+	//log_level := os.Getenv("LOG_LEVEL")
+	log_level := "debug"
+	switch {
+	case log_level == "debug":
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	case log_level == "info":
+		zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	case log_level == "warn":
+		zerolog.SetGlobalLevel(zerolog.WarnLevel)
+	case log_level == "error":
+		zerolog.SetGlobalLevel(zerolog.ErrorLevel)
+	case log_level == "fatal":
+		zerolog.SetGlobalLevel(zerolog.FatalLevel)
+	case log_level == "panic":
+		zerolog.SetGlobalLevel(zerolog.PanicLevel)
+	case log_level == "no":
+		zerolog.SetGlobalLevel(zerolog.NoLevel)
+	case log_level == "disabled":
+		zerolog.SetGlobalLevel(zerolog.Disabled)
+	case log_level == "trace":
+		zerolog.SetGlobalLevel(zerolog.TraceLevel)
+	default:
+		zerolog.SetGlobalLevel(zerolog.ErrorLevel)
+	}
+	// Log a human-friendly, colorized output
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+
+	
 	util_object := utils.Utils{Destination_path: destination_path}
 
 	// Photos
 	if process_photos {
 		var list_of_files []string
-		fmt.Println("Processing photos with date metadata")
+		
+		log.Debug().Msg("Processing photos with date metadata")
+		
 		err := filepath.Walk(source_path, func(path string, info os.FileInfo, err error) error {
 			if filepath.Ext(path) == ".jpg" {
 				list_of_files = append(list_of_files, path)
@@ -31,14 +70,14 @@ func main() {
 		})
 
 		if err != nil {
-			panic(err)
+			log.Error().Stack().Err(err).Msg("")
 		}
 
 		util_object.Get_folder_tree(list_of_files)
 		util_object.Create_folders_and_copy_files(false)
 
 		if len(util_object.Failed_files) > 0 {
-			fmt.Println("Processing photos with file name")
+			log.Debug().Msg("Processing photos with file name")
 			util_object.Get_folder_tree_with_name(util_object.Failed_files)
 			util_object.Create_folders_and_copy_files(false)
 		}
@@ -47,7 +86,7 @@ func main() {
 	// Videos
 	if process_videos {
 		var list_of_files []string
-		fmt.Println("Processing photos with date metadata")
+		log.Debug().Msg("Processing photos with date metadata")
 		err := filepath.Walk(source_path, func(path string, info os.FileInfo, err error) error {
 			if filepath.Ext(path) == ".mp4" {
 				list_of_files = append(list_of_files, path)
@@ -59,16 +98,18 @@ func main() {
 			panic(err)
 		}
 
-		fmt.Println("Processing videos with file name")
+		log.Debug().Msg("Processing videos with file name")
 		util_object.Get_folder_tree_with_name(list_of_files)
 		util_object.Create_folders_and_copy_files(true)
 	}
 
 	// Disply Fatal files list
 	if len(util_object.Fatal_files) > 0 {
-		fmt.Println("Could not process")
-		fmt.Printf("%v", util_object.Fatal_files)
+		err := errors.New("Could not process files")
+		log.Error().Stack().Err(err).Msgf("%v", util_object.Fatal_files)
 	} else {
-		fmt.Println("Success!")
+		log.Debug().Msg("Success!")
 	}
+
+	log.Debug().Msgf("Time Taken = %v", time.Since(start))
 }
